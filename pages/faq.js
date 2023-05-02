ab2a8852cf76dae6789af79c28e5efdcb112aa19
Guilherme Marucchi <guilherme.marucchi@gmail.com>
2023-05-02 15:47:47 -0300

    teste com primeiras páginas e navegação

---
+++ pages/faq.js
@@ -0,0 +1,8 @@
+export default function FAQPage () {
+    return (
+        <div>
+            <h1>Alura Cases - Página de perguntas</h1>
+            <a href='/'>Ir para home</a>
+        </div>
+    )
+}
