b3af036ab99f75db8a0ea5488f29bc14fab1acb5
Guilherme Marucchi <guilherme.marucchi@gmail.com>
2023-05-03 13:51:20 -0300

    alterar nome da pasta componentes para components

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,37 @@
-import React from 'react';
-import Link from '../src/componentes/Link';
-import Footer from '../src/componentes/patterns/Footer';
+import HomeScreen from "../src/screens/HomeScren";
+
+export default HomeScreen;
+
+// import React from 'react';
+// import Link from '../src/components/Link';
+// import Footer from '../src/components/patterns/Footer';
 
 
-function Title({ children, as }) {
-    const Tag = as;
-    return(
-    <React.Fragment>
-        <Tag>
-            {children}
-        </Tag>
-        <style jsx>{`
-            ${Tag} {
-                color: red;                
-            }
-        `}</style>
-    </React.Fragment>
+// function Title({ children, as }) {
+//     const Tag = as;
+//     return(
+//     <React.Fragment>
+//         <Tag>
+//             {children}
+//         </Tag>
+//         <style jsx>{`
+//             ${Tag} {
+//                 color: red;                
+//             }
+//         `}</style>
+//     </React.Fragment>
         
-    );
-}
+//     );
+// }
 
-export default function HomePage() {
-    return (
-        <div>            
-            <Title as="h1">Alura Cases - Home Page</Title>
-            <Link href="/faq">
-                Ir para a página de perguntas
-            </Link>
-            <Footer />
-        </div>
-    )
-}
+// export default function HomePage() {
+//     return (
+//         <div>            
+//             <Title as="h1">Alura Cases - Home Page</Title>
+//             <Link href="/faq">
+//                 Ir para a página de perguntas
+//             </Link>
+//             <Footer />
+//         </div>
+//     )
+// }
