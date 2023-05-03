cd4406bed1d7c016fd6fd1cdf99a7bc678d8100e
Guilherme Marucchi <guilherme.marucchi@gmail.com>
2023-05-03 13:46:38 -0300

    adiciona HomeScreen brabo e retira LinkEstilizado

--- src/componentes/Link/index.js
+++ src/componentes/Link/index.js
@@ -1,10 +1,9 @@
 import NextLink from 'next/link';
-import LinkEstilizado from '../LinkEstilizado';
 
 export default function Link ({children, href, ...props}) {
     return (
         <NextLink href={href} passHref legacyBehavior>
-            <LinkEstilizado {...props}>{children}</LinkEstilizado>
+            <a {...props}>{children}</a>
         </NextLink>
     )
 }
