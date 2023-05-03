cb93033f066d7dd9209d578a237c142622db245b
Guilherme Marucchi <guilherme.marucchi@gmail.com>
2023-05-03 10:51:58 -0300

    GlobalStyle e introdução da _app.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,38 @@
+import React from 'react';
 import Link from '../src/componentes/Link';
+
+function GlobalStyle() {
+    return(
+        <style global jsx>{`
+            body{
+                font-family: sans-serif;
+            }
+        `}</style>
+    );
+}
+
+function Title({ children, as }) {
+    const Tag = as;
+    return(
+    <React.Fragment>
+        <Tag>
+            {children}
+        </Tag>
+        <style jsx>{`
+            ${Tag} {
+                color: red;                
+            }
+        `}</style>
+    </React.Fragment>
+        
+    );
+}
 
 export default function HomePage() {
     return (
         <div>
-            <h1>Alura Cases - Home Page</h1>
+            <GlobalStyle />
+            <Title as="h1">Alura Cases - Home Page</Title>
             <Link href="/faq">
                 Ir para a página de perguntas
             </Link>
--- src/componentes/Link/index.js
+++ src/componentes/Link/index.js
@@ -4,7 +4,7 @@ import LinkEstilizado from '../LinkEstilizado';
 export default function Link ({children, href, ...props}) {
     return (
         <NextLink href={href} passHref legacyBehavior>
-        <LinkEstilizado {...props}>{children}</LinkEstilizado>
-    </NextLink>
+            <LinkEstilizado {...props}>{children}</LinkEstilizado>
+        </NextLink>
     )
 }
