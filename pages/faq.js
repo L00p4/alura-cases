9f39b4df5f2dfe2d71c8841682e0fcdf5484cca8
Guilherme Marucchi <guilherme.marucchi@gmail.com>
2023-05-04 10:32:31 -0300

    getStasticProps

--- pages/faq.js
+++ pages/faq.js
@@ -1,18 +1,38 @@
 import { useEffect, useState } from 'react'
 import Link from '../src/components/Link'
 
-export default function FAQPage () {
-    const [faq, setFaq] = useState([]);
-    useEffect(() => {
-        const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
-        fetch(FAQ_API_URL)
-            .then((respostaDoServidor) => {
-                return respostaDoServidor.json();
-            })
-            .then((resposta) => {
-                setFaq(resposta);
-            })
-    }, []);
+export async function getStaticProps() {
+    const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
+    const faq = await fetch(FAQ_API_URL)
+        .then((respostaDoServidor) => {
+            return respostaDoServidor.json();
+        })
+        .then((resposta) => {
+            return resposta;
+        });
+
+    return {
+        props: {
+            qualquercoisa: "qqr coisa que passar aqui vai aparecer em...",
+            faq,
+        }, // will be passed to the page component as props
+    };
+}
+
+
+export default function FAQPage ({ faq }) {
+    console.log(faq)
+    // const [faq, setFaq] = useState([]);
+    // useEffect(() => {
+    //     const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
+    //     fetch(FAQ_API_URL)
+    //         .then((respostaDoServidor) => {
+    //             return respostaDoServidor.json();
+    //         })
+    //         .then((resposta) => {
+    //             setFaq(resposta);
+    //         })
+    // }, []);
     return (
         <div>
             <h1>Alura Cases - Página de perguntas</h1>
