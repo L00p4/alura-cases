1ff69042b49a64e80fdb4b37f8f1bd388d60dec9
Guilherme Marucchi <guilherme.marucchi@gmail.com>
2023-05-04 11:15:13 -0300

    head/title e fonts

--- pages/faq.js
+++ pages/faq.js
@@ -1,7 +1,16 @@
-import { useEffect, useState } from 'react'
+import Head from 'next/head';
 import Link from '../src/components/Link'
 
+// SSG - Static Site Generation
+// SSR - Server Side Rendering
+// ISG - Incremental Static Generation (tipo uma mistura dos dois anteriores)
+
+// export async function getServerSideProps() {
+//     console.log('Em modo DEV, sempre roda! A cada acesso')
+//     console.log('Rodando a cada acesso que você recebe')
 export async function getStaticProps() {
+    console.log('Em modo DEV, sempre roda! A cada acesso');   
+    console.log('Roda SOMENTE em build time');
     const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
     const faq = await fetch(FAQ_API_URL)
         .then((respostaDoServidor) => {
@@ -21,20 +30,12 @@ export async function getStaticProps() {
 
 
 export default function FAQPage ({ faq }) {
-    console.log(faq)
-    // const [faq, setFaq] = useState([]);
-    // useEffect(() => {
-    //     const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
-    //     fetch(FAQ_API_URL)
-    //         .then((respostaDoServidor) => {
-    //             return respostaDoServidor.json();
-    //         })
-    //         .then((resposta) => {
-    //             setFaq(resposta);
-    //         })
-    // }, []);
+    console.log('Isso roda no servidor?')
     return (
         <div>
+            <Head>
+                <title>FAQ - Perguntas Brabas</title>
+            </Head>
             <h1>Alura Cases - Página de perguntas</h1>
             <Link href='/'>
                 Ir para home
